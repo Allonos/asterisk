f02336a63daaf815f18d2dce8325ea3a4413aa5c
rezi tsitsvidze <r.tsitsvidze@optimogroup.io>
2025-10-05 12:47:06 +0400

    feat: new-3d-model

--- src/pages/DataAnalysis.jsx
+++ src/pages/DataAnalysis.jsx
@@ -1,7 +1,25 @@
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, useGLTF, PerspectiveCamera } from "@react-three/drei";
+
+function Model() {
+  const { scene } = useGLTF("/src/assets/solar_system_asteroids.glb");
+  return <primitive object={scene} rotation={[0, Math.PI / 2, 0]} />;
+}
+
 const DataAnalysis = () => {
   return (
-    <div>
-      <h1>hello</h1>
+    <div style={{ width: "100%", height: "100vh", backgroundColor: "black" }}>
+      <Canvas>
+        <PerspectiveCamera makeDefault position={[15, -30, -10]} />
+        <ambientLight intensity={1} />
+        <directionalLight position={[5, 5, 3]} intensity={10} />
+        <pointLight position={[-5, -5, -3]} intensity={10} />
+        <Suspense fallback={null}>
+          <Model />
+        </Suspense>
+        <OrbitControls enableZoom={true} enablePan={true} enableRotate={true} />
+      </Canvas>
     </div>
   );
 };
