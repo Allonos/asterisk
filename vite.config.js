2947b59f0387862cf67e35552a33c515973797d7
rezi tsitsvidze <r.tsitsvidze@optimogroup.io>
2025-10-06 11:53:00 +0400

    feat: publication

--- vite.config.js
+++ vite.config.js
@@ -3,30 +3,17 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { fileURLToPath } from "url";
 import { dirname, resolve } from "path";
-import { viteStaticCopy } from "vite-plugin-static-copy";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default defineConfig({
   base: "/asterisk/",
-  plugins: [
-    react(),
-    tailwindcss(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: "public/assets/*", // copy all files in assets
-          dest: "assets", // into dist/assets/
-        },
-      ],
-    }),
-  ],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": resolve(__dirname, "./src"),
     },
   },
-  // Ensure proper asset handling
   assetsInclude: ["**/*.glb", "**/*.gltf"],
 });
